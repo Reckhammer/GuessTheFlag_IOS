b8ba873c517a7b322afa2afb060084f0724c52c0
Reckhammer <joshua.rechkemmer48@gmail.com>
2024-02-29 18:32:15 -0600

    Creates flag buttons, Shows alert whether guess is right or wrong

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -7,18 +7,81 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct ContentView: View 
+{
+    @State private var showingScore = false;
+    @State private var scoreTitle = "";
+    
+    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Spain", "UK", "US"].shuffled();
+    @State var correctAnswer = Int.random(in: 0...2);
+    
+    var body: some View
+    {
+        ZStack
+        {
+            Color.blue
+                .ignoresSafeArea();
+            
+            VStack(spacing: 30)
+            {
+                VStack
+                {
+                    Text("Tap the Flag of")
+                        .foregroundStyle(.white);
+                    
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(.white);
+                }
+                
+                
+                ForEach(0..<3)
+                {
+                    number in
+                    
+                    Button
+                    {
+                        flagTapped(number);
+                    }
+                label:
+                    {
+                        Image(countries[number]);
+                    }
+                    
+                }
+            }
         }
-        .padding()
+        .alert(scoreTitle, isPresented: $showingScore)
+        {
+            Button("Continue", action: askQuestion);
+        }
+    message:
+        {
+            Text("Your score is ???");
+        }
+    }
+    
+    func flagTapped(_ number: Int)
+    {
+        if number == correctAnswer
+        {
+            scoreTitle = "Correct";
+        }
+        else
+        {
+            scoreTitle = "Wrong";
+        }
+        
+        showingScore = true;
+    }
+    
+    func askQuestion()
+    {
+        countries.shuffle();
+        correctAnswer = Int.random(in: 0...2);
     }
 }
 
-#Preview {
+#Preview 
+{
     ContentView()
 }
