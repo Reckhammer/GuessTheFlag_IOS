fe02b806e6a7f0f0306acea66d7eff62e4ccaab5
Reckhammer <joshua.rechkemmer48@gmail.com>
2024-02-29 18:30:53 -0600

    Initial project commit

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Joshua Rechkemmer on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
